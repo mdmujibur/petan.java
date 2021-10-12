d7873acf3a6b197b8b9f913f5e24fe84fea95000
mujibur <mujibur777888@gmail.com>
2021-10-12 04:48:51 -1200

    we have creat two file 1 rof statements or 1 math.function

---
+++ mathfunction.js
@@ -0,0 +1,26 @@
+// what is math for js
+//maht is  a object function for counting
+var b = 257
+var a = 15
+console.log(Math.E)
+console.log(Math.PI);
+
+console.log(Math.abs(a));
+console.log(Math.floor(a));
+console.log(Math.ceil(a));
+console.log(Math.round(a));
+
+//when we need to count min or max number wecan used math function
+console.log(Math.min(a, b));
+console.log(Math.max(a, b));
+//when we need to count we can used Math.pow esily 
+console.log(Math.pow(2, 7));
+console.log(Math.pow(3, 5));
+
+
+//when we need to count sure root we used Math,sqrt esily 
+console.log(Math.sqrt(35, 9));
+console.log(Math.sqrt(5, 9));
+
+console.log(Math.round(Math.random() * b + 1))
+    // this is the consept for math function
---
+++ statement.js
@@ -0,0 +1,2 @@
+//what is statements
+//statements is a line this line have start or close that is call statements for programming langous
